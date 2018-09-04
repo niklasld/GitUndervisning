724a3da8c80104a4da7e72677437033d489087e6
mrtauern <tauern.vs@gmail.com>
2018-09-04 11:40:23 +0200

    Dig Hassan.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Felix er gruppe medlem 3");
-        System.out.println("Dig Hassan");
+        System.out.println("Dig Hassan.");
 
     }
 }
